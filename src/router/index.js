baee19cbef3e04c31d46ba2606836e4eecd277ba
gaoxiang <igaoxian@foxmail.com>
2020-10-27 13:31:54 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,29 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import Home from '@/view/common/home'
 import Test from '@/view/common/test'
 
 class AppRouter extends React.Component {
     render() {
         return (
-            // <Home />
-            <BrowserRouter>
-                <Route path="/" component={Home} />
-                <Route path="/test" component={Test} />
-            </BrowserRouter>
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Form</Link>
+                        </li>
+                        <li>
+                            <Link to="/one">One</Link>
+                        </li>
+                        <li>
+                            <Link to="/two">Two</Link>
+                        </li>
+                    </ul>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/one" render={() => <h3>One</h3>} />
+                    <Route path="/two" render={() => <h3>Two</h3>} />
+                </div>
+            </Router>
         )
     }
 }
